4b87423fd0bb2f002ea95c2d007d8680704db671
ckhckh2 <gusrudchl12@gmail.com>
2023-12-16 18:34:57 +0900

    수정

--- src/main/java/com/std/sbb/domain/taste/form/TasteForm.java
+++ src/main/java/com/std/sbb/domain/taste/form/TasteForm.java
@@ -1,14 +1,19 @@
 package com.std.sbb.domain.taste.form;
 
 import com.std.sbb.global.jpa.BaseEntity;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.Getter;
 
 @Data
 @Getter
 public class TasteForm extends BaseEntity {
+    @NotEmpty
     private Integer sweet;
+    @NotEmpty
     private Integer body;
+    @NotEmpty
     private Integer acidity;
+    @NotEmpty
     private Integer tannin;
 }

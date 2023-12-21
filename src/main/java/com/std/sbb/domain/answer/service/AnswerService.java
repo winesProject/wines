015566754b6362a8131ee969948648f040c6c740
yuuujin33 <yuuujin9922@gmail.com>
2023-12-21 13:05:54 +0900

    이메일 전송, 문의 게시판 완료

--- src/main/java/com/std/sbb/domain/answer/service/AnswerService.java
+++ src/main/java/com/std/sbb/domain/answer/service/AnswerService.java
@@ -1,11 +1,25 @@
 package com.std.sbb.domain.answer.service;
 
+import com.std.sbb.domain.answer.entity.Answer;
 import com.std.sbb.domain.answer.repository.AnswerRepository;
+import com.std.sbb.domain.question.entity.Question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class AnswerService {
     private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content) {
+        Answer answer = Answer
+                .builder()
+                .content(content)
+                .question(question)
+                .createDate(LocalDateTime.now())
+                .build();
+        this.answerRepository.save(answer);
+    }
 }
--- src/main/java/com/std/sbb/domain/question/controller/QuestionController.java
+++ src/main/java/com/std/sbb/domain/question/controller/QuestionController.java
@@ -1,48 +1,65 @@
 package com.std.sbb.domain.question.controller;
 
+import com.std.sbb.domain.answer.form.AnswerForm;
+import com.std.sbb.domain.member.entity.Member;
+import com.std.sbb.domain.member.service.MemberService;
 import com.std.sbb.domain.question.entity.Question;
 import com.std.sbb.domain.question.form.QuestionForm;
 import com.std.sbb.domain.question.service.QuestionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/question")
 public class QuestionController {
     private final QuestionService questionservice;
+    private final MemberService memberService;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Question> questionList = this.questionservice.getList();
-        model.addAttribute("questionList", questionList);
+    public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
+        Page<Question> paging = this.questionservice.getList(page);
+        model.addAttribute("paging", paging);
         return "question_list";
     }
 
     @GetMapping("/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Long id) {
+    public String detail(Model model, @PathVariable("id") Long id, AnswerForm answerForm) {
         Question question = this.questionservice.getId(id);
         model.addAttribute("question",question);
         return "question_detail";
     }
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/create")
     public String questionCreate (QuestionForm questionForm) {
         return "question_form";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create")
-    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult
-                                ) {
-        this.questionservice.create(questionForm.getArticle(),questionForm.getContent());
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult,
+                                 Principal principal) {
+        if(bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        Member member = this.memberService.getMember(principal.getName());
+        this.questionservice.create(questionForm.getArticle(),questionForm.getContent(), member);
+        return "redirect:/question/list";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id, Principal principal) {
+        Question question = this.questionservice.getId(id);
+        this.questionservice.delete(question);
         return "redirect:/question/list";
     }
 }

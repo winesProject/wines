17988e3c7252f3c1412041ec20db6a10e3be6ee7
choiKYeon <gusrudchl12@gmail.com>
2023-12-27 18:45:45 +0900

    비밀번호 인증 및 발송 성공

--- src/main/java/com/std/sbb/domain/member/service/MemberService.java
+++ src/main/java/com/std/sbb/domain/member/service/MemberService.java
@@ -34,43 +34,6 @@ public class MemberService {
         return memberRepository.save(member);
     }
 
-
-    public boolean checkEmail(String memberEmail) {
-
-        /* 이메일이 존재하면 true, 이메일이 없으면 false  */
-        return memberRepository.existsByEmail(memberEmail);
-    }
-
-    public String getTmpPassword() {
-        char[] charSet = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-                'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-
-        String pwd = "";
-
-        /* 문자 배열 길이의 값을 랜덤으로 10개를 뽑아 조합 */
-        int idx = 0;
-        for (int i = 0; i < 10; i++) {
-            idx = (int) (charSet.length * Math.random());
-            pwd += charSet[idx];
-        }
-
-        return pwd;
-    }
-
-    public void updatePassword(String tmpPassword, String memberEmail) {
-
-        String encryptPassword = passwordEncoder.encode(tmpPassword);
-        Member member = memberRepository.findByEmail(memberEmail).orElseThrow(() ->
-                new IllegalArgumentException("해당 사용자가 존재하지 않습니다."));
-
-        member.updatePassword(encryptPassword);
-    }
-
-    private Optional<Member> findByEmail(String email) {
-        return memberRepository.findByEmail(email);
-    }
-
     @Transactional
     public Member whenSocialLogin(String providerTypeCode, String name, String username, String profileImgUrl) {
         Optional<Member> opMember = findByUsername(username);
@@ -80,6 +43,7 @@ public class MemberService {
         // 소셜 로그인를 통한 가입시 비번은 없다.
         return join(name, "", username, null, null, null, null, profileImgUrl); // 최초 로그인 시 딱 한번 실행
     }
+
     public Optional<Member> findByUsername(String username) {
         return memberRepository.findByUsername(username);
     }
@@ -88,27 +52,39 @@ public class MemberService {
         Optional<Member> member = this.memberRepository.findByUsername(username);
         return member.get();
     }
+
     public boolean toggleHeart(Long id, String username) {
         Optional<Member> memberOptional = findByUsername(username);
         Optional<Wine> wineOptional = wineRepository.findById(id);
 
-            Member member = memberOptional.get();
-            Wine wine = wineOptional.get();
+        Member member = memberOptional.get();
+        Wine wine = wineOptional.get();
 
-            boolean isLiked = wine.getMember().contains(member);
+        boolean isLiked = wine.getMember().contains(member);
 
-            if (isLiked) {
-                wine.getMember().remove(member);
-            } else {
-                wine.getMember().add(member);
-            }
-            wineRepository.save(wine);
+        if (isLiked) {
+            wine.getMember().remove(member);
+        } else {
+            wine.getMember().add(member);
+        }
+        wineRepository.save(wine);
 
-            return !isLiked;
+        return !isLiked;
     }
 
     public String getUsernameByNameAndEmail(String name, String email) {
         Member member = memberRepository.findByNameAndEmail(name, email);
         return (member != null) ? member.getUsername() : null;
     }
+
+    public boolean memberEmailCheck(String username, String Email) {
+
+        Member member = memberRepository.findByEmail(Email);
+        if (member != null && member.getName().equals(username)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }

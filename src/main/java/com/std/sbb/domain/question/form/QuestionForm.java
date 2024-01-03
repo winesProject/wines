e27604045893a615eaeec929e6f552a45e88f8ef
yuuujin33 <yuuujin9922@gmail.com>
2024-01-03 19:32:08 +0900

    문의 등록  오류 수정

--- src/main/java/com/std/sbb/domain/question/form/QuestionForm.java
+++ src/main/java/com/std/sbb/domain/question/form/QuestionForm.java
@@ -1,16 +1,16 @@
 package com.std.sbb.domain.question.form;
 
 import com.std.sbb.global.jpa.BaseEntity;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class QuestionForm extends BaseEntity {
-    @NotNull(message = "제목은 필수항목입니다.")
+    @NotEmpty(message = "제목은 필수항목입니다.")
     private String article;
 
-    @NotNull(message = "내용은 필수항목입니다.")
+    @NotEmpty(message = "내용은 필수항목입니다.")
     private String content;
 }

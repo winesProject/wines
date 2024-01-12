57cca6438288a6175f1744858ccb4ac1d0b1e1b7
choiKYeon <gusrudchl12@gmail.com>
2024-01-12 12:48:43 +0900

    sfd

--- src/main/java/com/std/sbb/global/imagesfile/config/WebMvcConfiguration.java
+++ src/main/java/com/std/sbb/global/imagesfile/config/WebMvcConfiguration.java
@@ -1,13 +1,24 @@
 package com.std.sbb.global.imagesfile.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:///D:/wines/src/main/resources/static/images/");
+
+        @Value("${myapp.image-path}")
+        private String imagePath;
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/images/**")
+                    .addResourceLocations(imagePath);
+
+//        .addResourceLocations("file:///Users/dev/Desktop/java/wines/src/main/resources/static/images/");
+//        .addResourceLocations("file:///Users/yujin/Desktop/work/wines/src/main/resources/static/images/");
     }
 }
+//    file:///C:/java_work/wines/src/main/resources/static/images/
